dae1b331a783da3952fd5367d19d352b495c37df
AnnonAB <annonab@null>
2016-10-12 14:05:50 +1030

    bigboy

--- bot.js
+++ bot.js
@@ -11,7 +11,15 @@ bot.onText(/\/biggestbab/,
   function (msg) {
     console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
     var fromId = msg.chat.id;
-    bot.sendMessage(fromId, "Matty is the biggest bab!");
+    bot.sendMessage(fromId, "*Matty* is the biggest bab!", {parse_mode:"Markdown"});
+  }
+);
+
+bot.onText(/\/biggestboy/,
+  function (msg) {
+    console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
+    var fromId = msg.chat.id;
+    bot.sendMessage(fromId, "_looks around_ I dunno little one, I dont see any big kids around here.", {parse_mode:"Markdown"});
   }
 );
 
@@ -19,6 +27,6 @@ bot.onText(/\/cutestbab/,
   function (msg) {
     console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
     var fromId = msg.chat.id;
-    bot.sendMessage(fromId, msg.from.first_name + " " + msg.from.last_name + " is the cutest bab!");
+    bot.sendMessage(fromId, "*" + msg.from.first_name + " " + msg.from.last_name + "* is the cutest bab!", {parse_mode:"Markdown"});
   }
 );
