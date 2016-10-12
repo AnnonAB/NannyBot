1d0ab20ddc4293aa68fbe1ba4533237a023cc3f0
AnnonAB <annonab@null>
2016-10-12 15:45:31 +1030

    Merged the Yaks Diff

--- bot.js
+++ bot.js
@@ -1,45 +1,65 @@
 var TelegramBot = require('node-telegram-bot-api');
 
 var t = require('./token.json');
-var bot = new TelegramBot(t.token, {polling: true});
+var bot = new TelegramBot(t.token, {
+    polling: true
+});
 
 var config = require('./config.json');
 
 
 
 bot.onText(/\/biggestbab/,
-  function (msg) {
-    console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
-    var fromId = msg.chat.id;
-    bot.sendMessage(fromId, "*Matty* is the biggest bab!", {parse_mode:"Markdown"});
-  }
+    function(msg) {
+        console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
+        var fromId = msg.chat.id;
+        bot.sendMessage(fromId, "*Matty* is the biggest bab!", {
+            parse_mode: "Markdown"
+        });
+    }
 );
 
 bot.onText(/\/biggestboy/,
-  function (msg) {
-    console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
-    var fromId = msg.chat.id;
-    bot.sendMessage(fromId, "_looks around_ I dunno little one, I dont see any big kids around here.", {parse_mode:"Markdown"});
-  }
+    function(msg) {
+        console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
+        var fromId = msg.chat.id;
+        bot.sendMessage(fromId, "_looks around_\nI dunno little one, I dont see any big kids around here.", {
+            parse_mode: "Markdown"
+        });
+    }
 );
 
+bot.onText(/\/oneandonlybigboy/,
+    function(msg) {
+        console.log("Received command from: %s:%s", msg.chat.title, msg.from.username)
+        var fromId = msg.chat.id;
+        bot.sendMessage(fromId, "*Elliot* is a coding troll who thinks he is a big boy", {
+            parse_mode: "Markdown"
+        });
+    }
+);
+
+
 bot.onText(/\/cutestbab/,
-  function (msg) {
-    console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
-    
-    var fromId = msg.chat.id;
+    function(msg) {
+        console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
 
-    if (typeof msg.reply_to_message !== "undefined") {
-      console.log(msg.reply_to_message.from);
+        var fromId = msg.chat.id;
 
-      var lastname = typeof msg.reply_to_message.from.last_name !== "undefined" ? " " + msg.reply_to_message.from.last_name : "";
-      bot.sendMessage(fromId, "*" + msg.reply_to_message.from.first_name + lastname + "* is the cutest bab!", {parse_mode:"Markdown"});
+        if (typeof msg.reply_to_message !== "undefined") {
+            console.log(msg.reply_to_message.from);
+
+            var lastname = typeof msg.reply_to_message.from.last_name !== "undefined" ? " " + msg.reply_to_message.from.last_name : "";
+            bot.sendMessage(fromId, "*" + msg.reply_to_message.from.first_name + lastname + "* is the cutest bab!", {
+                parse_mode: "Markdown"
+            });
+        } else {
+            console.log(msg.from);
+
+            var lastname = typeof msg.from.last_name !== "undefined" ? " " + msg.from.last_name : "";
+            bot.sendMessage(fromId, "*" + msg.from.first_name + lastname + "* is the cutest bab!", {
+                parse_mode: "Markdown"
+            });
+        }
     }
-    else {
-      console.log(msg.from);
-
-      var lastname = typeof msg.from.last_name !== "undefined" ? " " + msg.from.last_name : "";
-      bot.sendMessage(fromId, "*" + msg.from.first_name + lastname + "* is the cutest bab!", {parse_mode:"Markdown"}                     );
-    }
-  }
 );
