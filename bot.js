88c4fa2b5e89019321e9810c84739cb1910e3d1d
AnnonAB <annonab@null>
2016-11-16 13:38:14 +1030

    Now with persistant scoring!

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
+/**** NPM Module declerations ****/
+
 var TelegramBot = require('node-telegram-bot-api');
 var swearjar = require('swearjar');
+var moment = require('moment');
+var fs = require('fs');
+/*********************************/
 
 var t = require('./token.json');
 var config = require('./config.json');
+
+fs.writeFile('./config.json', JSON.stringify(config), 'utf8'); //write out config once an hour
 
 var bot = new TelegramBot(t.token, {
     polling: true
@@ -21,10 +28,14 @@ function include(f) {
 
 var commandArray = [];
 var functionArray = [];
-var scoreRecords = [];
 var dailyScore = [];
+var scriptStarted = moment([]);
 
-function userScore(userID, score, lastMessage) {
+if (typeof config.scoreRecords === 'undefined') {
+    config.scoreRecords = [];
+}
+
+function userScore(userID, score, lastMessage, lastDate) {
     this.userID = userID;
     this.score = score;
     this.lastMessage = lastMessage;
@@ -39,6 +50,9 @@ function addFunctionListener(command, functionName) // Example: addFunctionListe
 
 
 /***** uptime *****/
+
+
+
 var intervalId = setInterval(timer, 1000); //Runs Every Second
 var years = 0;
 var weeks = 0;
@@ -50,13 +64,19 @@ var seconds = 0;
 function timer() {
     seconds++;
     if (seconds > 59) { seconds = 0; minutes++; }
-    if (minutes > 59) { minutes = 0; hours++; }
+    if (minutes > 59) {
+        minutes = 0;
+        fs.writeFile('./config.json', JSON.stringify(config), 'utf8'); //write out config once an hour
+        hours++;
+    }
     if (hours > 23) { hours = 0; days++; dailyScore = null;}
     if (days > 6) { days = 0; weeks++; }
     if (weeks > 51) { weeks = 0; years++; }
 }
 
+
 bot.onText(/\/uptime/, function(msg) {
+
     console.log("Recieved command from: %s:%s", msg.chat.title, msg.from.username);
     var fromId = msg.chat.id;
     bot.sendMessage(fromId, "Since my Last Restart I have Been active for: \n ```" + years + " Years\n" + weeks + " Weeks\n" + days + " Days\n" + hours + " Hours\n" + minutes + " Minutes\n" + seconds + " Seconds```", {
@@ -96,22 +116,26 @@ bot.onText(/\/biggestbab/,
 bot.onText(/\/myscore/, function(msg) {
         console.log("Received command from: %s:%s", msg.chat.title, msg.from.username);
         var fromId = msg.chat.id;
-        var found = false;
         var lastname = typeof msg.from.last_name !== "undefined" ? " " + msg.from.last_name : "";
-        for (var i = 0; i < scoreRecords.length; i++) {
-            if (scoreRecords[i].userID === msg.from.id) {
-                found = true;
-                bot.sendMessage(fromId, "*" + msg.from.first_name + lastname + "*, you're such a pottymouth! Your current score is *" + scoreRecords[i].score + "*\n I last caught you saying: " + scoreRecords[i].lastMessage , {
-                    parse_mode: "Markdown"
-                });
-                break;
+
+        var scoreRecordIndex = arrayObjectIndexOf(config.scoreRecords, config.scoreRecords.userID, msg.from.id);
+        var dailyScoreIndex = arrayObjectIndexOf(dailyScore, dailyScore.userID, msg.from.id);
+
+        var messageToSend;
+
+        if (dailyScoreIndex === -1) {
+            if (scoreRecordIndex === -1) {
+                messageToSend = "*Amazing!* " + msg.from.first_name + lastname +  " I have never heard you swear before. You get the biggest _hugs_ for being sooo good.";
+            } else {
+                messageToSend = "*_Wow!_* " + msg.from.first_name + lastname +  " you're a good and well behaved bab. I haven't caught any naughty words coming from your mouth today. ";
+                messageToSend += "But I have caught you swearing in the past.\n\nYour total score is *" + config.scoreRecords[scoreRecordIndex].score + "*.\n\nKeep up the good work!";
             }
         }
-        if (found === false) {
-            bot.sendMessage(fromId, "*_Wow!_* " + msg.from.first_name + lastname +  " you're a good and well behaved bab. I have never caught a naughty word come out your mouth!", {
-                parse_mode: "Markdown"
-            });
+        else {
+            messageToSend = "*" + msg.from.first_name + lastname + "*, you're such a pottymouth!\n\nYour current score for today is *" + dailyScore[dailyScoreIndex].score;
+            messageToSend += "*.\nYour total score is *" + config.scoreRecords[scoreRecordIndex].score + "*.\nI last caught you saying:\n" + dailyScore[dailyScoreIndex].lastMessage;
         }
+        bot.sendMessage(fromId, messageToSend, { parse_mode: "Markdown"});
     }
 );
 
@@ -190,28 +214,41 @@ bot.onText(/(.+)/, function(msg, match) {
 
     for (i in score) {
         if (score.hasOwnProperty(i)) {
-            sum += score[i]
+            sum += score[i];
         }
     }
 
     if (sum > 0) {
         var found = false;
-        for (var i = 0; i < scoreRecords.length; i++){
-            if (scoreRecords[i].userID === msg.from.id) {
-                found = true;
-                scoreRecords[i].score += sum;
-                scoreRecords[i].lastMessage = match[0];
-                console.log("%s swore again! current score: %s, last message was: %s", msg.from.username, scoreRecords[i].score, scoreRecords[i].lastMessage);
-                break;
+        var scoreRecordIndex = arrayObjectIndexOf(config.scoreRecords, config.scoreRecords.userID, msg.from.id);
+        var dailyScoreIndex = arrayObjectIndexOf(dailyScore, dailyScore.userID, msg.from.id);
+
+        if (scoreRecordIndex === -1) {
+            config.scoreRecords.push(new userScore(msg.from.id, sum, match[0]));
+            dailyScore.push(new userScore(msg.from.id, sum, match[0]));
+            console.log("%s swore for the very first time!\ncurrent score: %s\nlast message was %s", msg.from.username, config.scoreRecords[config.scoreRecords.length - 1].score, config.scoreRecords[config.scoreRecords.length - 1].lastMessage);
+        } else {
+            config.scoreRecords[scoreRecordIndex].score += sum;
+            if (dailyScoreIndex === -1) {
+                dailyScore.push(new userScore(msg.from.id, sum, match[0]));
+                console.log("%s swore for the first time today!\ncurrent score: %s\ntotal score: %s\nlast message was:\n%s", msg.from.username, dailyScore[dailyScore.length - 1].score, config.scoreRecords[scoreRecordIndex].score, dailyScore[dailyScore.length - 1].lastMessage);
+            } else {
+                dailyScore[dailyScoreIndex].score += sum;
+                dailyScore[dailyScoreIndex].lastMessage = match[0];
+
+                console.log("%s swore again today!\ncurrent score: %s\ntotal score: %s\nlast message was:\n%s", msg.from.username, dailyScore[dailyScoreIndex].score, config.scoreRecords[scoreRecordIndex].score, dailyScore[dailyScoreIndex].lastMessage);
             }
         }
-
-        if (found === false) {
-            scoreRecords.push(new userScore(msg.from.id, sum, match[0]));
-            console.log("%s swore for the first time! current score: %s, last message was %s", msg.from.username, scoreRecords[scoreRecords.length - 1].score, scoreRecords[scoreRecords.length - 1].lastMessage);
-        }
+        fs.writeFile('./config.json', JSON.stringify(config), 'utf8'); //write out config once an hour
     }
 });
+
+function arrayObjectIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+}
 
 
 addFunctionListener("/testFunc", testFunc);
