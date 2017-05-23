38aa736d2024a71fa3c445893d4f332bc04ec2fa
Skylar Flare <sky.flare@hotmail.com>
2017-05-23 20:07:33 +1000

    Added 8ball command

--- bot.js
+++ bot.js
@@ -460,6 +460,24 @@ bot.onText(/\/google (.+)/i,
 );
 
 
+// 8Ball Command
+bot.onText(/\/8ball (.+)/i,
+	function(msgObject)
+	{
+		let responseList = [
+			"It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+			"You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
+			"Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+			"Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+			"My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+		
+		let randomChoice = responseList[Math.floor(Math.random() * responseList.length)];
+		
+		say(msgObject, randomChoice);
+	}
+);
+
+
 /************ Various Funcitons *************/
 
 // returns the index of an object in an array by a search of one of the objects properties.
